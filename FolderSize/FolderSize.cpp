c6a5d1fa9dd942ab2f5bf56bb6d1ba323397cd2a
Arty <33781032+ItzArty@users.noreply.github.com>
2022-11-18 23:19:54 +0100

    Uploaded project files

---
+++ FolderSize/FolderSize.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <filesystem>
+#include <chrono>
+
+using namespace std;
+using namespace std::filesystem;
+using namespace chrono;
+
+int main( int argc, char** args ) {
+
+	if (argc < 2) {
+
+		cout << "Usage: " << args[0] << " [path]" << endl;
+
+		return 0;
+
+	}
+
+	path target = args[1];
+
+	if (!exists(target)) {
+
+		cout << "Entered path does not exist." << endl;
+
+		return 0;
+
+	}
+
+	unsigned long long totalSize = 0;
+
+	steady_clock::time_point start = steady_clock::now();
+
+	for ( auto const& dir_entry : recursive_directory_iterator(target, directory_options::skip_permission_denied) ) {
+
+		try {
+
+			if (!dir_entry.is_directory()) totalSize += dir_entry.file_size();
+
+		} catch (exception& e) {
+
+			cout << e.what() << endl;
+
+		}
+
+	}
+
+	steady_clock::time_point end = steady_clock::now();
+
+	cout << "Path evaluated: " << target.string() << endl;
+	cout << "Total size: " << ( totalSize / ( 1024 * 1024 ) ) << "MB" << endl;
+	cout << "Time elapsed: " << duration_cast<milliseconds>(end - start).count() << "ms" << endl;
+
+	return 0;
+
+}
